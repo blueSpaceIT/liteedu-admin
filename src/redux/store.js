87579ef93c7bff47924dff9586072afed01d3e5f
Shakilofficial <mrshakilhossainofficial@gmail.com>
2026-01-21 19:54:53 +0600

    Update

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,10 @@ import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "./features/Api/Auth/AuthSlice";
 import { apiSlice } from "./features/slice/baseUrl";
 
-
 export const store = configureStore({
-  reducer: {
-    [apiSlice.reducerPath]: apiSlice.reducer,
-    [authSlice.reducerPath]: authSlice.reducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(apiSlice.middleware),
+    reducer: {
+        [apiSlice.reducerPath]: apiSlice.reducer,
+        [authSlice.reducerPath]: authSlice.reducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
 });
