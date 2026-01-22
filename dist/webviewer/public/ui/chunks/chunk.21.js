8e180c61d43ff5f7571a810e39f58a1daa5decfe
Shakilofficial <mrshakilhossainofficial@gmail.com>
2026-01-22 09:41:56 +0600

    Fix

--- dist/webviewer/public/ui/chunks/chunk.21.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{1662:function(e,n,a){var p=a(55),r=a(625);p({target:"Number",stat:!0,forced:Number.parseInt!=r},{parseInt:r})}}]);
-//# sourceMappingURL=chunk.21.js.map
