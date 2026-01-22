05a4758e86d2291ab833603e4e5a66bf24830ebb
MD RUBEL <mdrubel29879@gmail.com>
2026-01-22 14:57:03 +0600

    fix:import error fixed

--- src/components/teachers/teachersTable.jsx
+++ src/components/teachers/teachersTable.jsx
@@ -1,38 +1,38 @@
 /* eslint-disable react/prop-types */
-import TeacherRow from "./TeacherRow";
+import TeacherRow from "./teacherRow";
 
 const TeacherTable = ({ teachers, onEdit, onDelete }) => {
-  return (
-    <div className="overflow-x-auto rounded-lg border border-gray-300 dark:border-gray-700 shadow">
-      <table className="min-w-full table-auto border-collapse">
-        <thead className="bg-gray-200 dark:bg-gray-700 dark:text-white">
-          <tr>
-            <th className="px-2 py-3 text-center">#</th>
-            <th className="px-2 py-3 text-left">Name</th>
-            <th className="px-2 py-3 text-left">Phone</th>
-            <th className="px-2 py-3 text-left">Email</th>
-            <th className="px-2 py-3 text-left">Gender</th>
-            <th className="px-2 py-3 text-left">Education (HSC)</th>
-            <th className="px-2 py-3 text-left">Education (MBBS)</th>
-            <th className="px-2 py-3 text-left">Session</th>
-            <th className="px-2 py-3 text-center">Status</th>
-            <th className="px-2 py-3 text-center">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {teachers.map((teacher, index) => (
-            <TeacherRow
-              key={teacher._id}
-              teacher={teacher}
-              index={index}
-              onEdit={onEdit}
-              onDelete={onDelete}
-            />
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+    return (
+        <div className="overflow-x-auto rounded-lg border border-gray-300 shadow dark:border-gray-700">
+            <table className="min-w-full table-auto border-collapse">
+                <thead className="bg-gray-200 dark:bg-gray-700 dark:text-white">
+                    <tr>
+                        <th className="px-2 py-3 text-center">#</th>
+                        <th className="px-2 py-3 text-left">Name</th>
+                        <th className="px-2 py-3 text-left">Phone</th>
+                        <th className="px-2 py-3 text-left">Email</th>
+                        <th className="px-2 py-3 text-left">Gender</th>
+                        <th className="px-2 py-3 text-left">Education (HSC)</th>
+                        <th className="px-2 py-3 text-left">Education (MBBS)</th>
+                        <th className="px-2 py-3 text-left">Session</th>
+                        <th className="px-2 py-3 text-center">Status</th>
+                        <th className="px-2 py-3 text-center">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {teachers.map((teacher, index) => (
+                        <TeacherRow
+                            key={teacher._id}
+                            teacher={teacher}
+                            index={index}
+                            onEdit={onEdit}
+                            onDelete={onDelete}
+                        />
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 };
 
 export default TeacherTable;
